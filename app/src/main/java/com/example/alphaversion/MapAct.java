fa849b67d6f7dd384ab597f34e49441302511a25
NoamVak <noamvak765@gmail.com>
2021-11-15 13:21:22 +0200

    need to continue

---
+++ app/src/main/java/com/example/alphaversion/MapAct.java
@@ -0,0 +1,14 @@
+package com.example.alphaversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
