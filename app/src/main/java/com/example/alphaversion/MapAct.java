c98f9704e2a456d0cc690c7b378c31ee19dfd8ab
NoamVak <noamvak765@gmail.com>
2021-11-17 16:24:54 +0200

    need to continue

--- app/src/main/java/com/example/alphaversion/MapAct.java
+++ app/src/main/java/com/example/alphaversion/MapAct.java
@@ -3,12 +3,23 @@ package com.example.alphaversion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.MapView;
 
 public class MapAct extends AppCompatActivity {
-
+    MapView mpV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        mpV=(MapView) findViewById(R.id.mpV);
+
+    }
+
+    public void location(View view) {
+
+
     }
 }
